e2a2bc343c2248b1ac86ec9cd6334366f6948afe
Alisher <alisherxujanov163@gmail.com>
2020-11-15 14:39:47 +0300

    Resizing images

--- main/routes.py
+++ main/routes.py
@@ -4,6 +4,7 @@ from main.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from main.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 import secrets, os
+from PIL import Image
 
 
 posts = [
@@ -75,7 +76,11 @@ def save_picture(form_picture):
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext 
     picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
-    form_picture.save(picture_path)
+
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
 
     return picture_fn
 
