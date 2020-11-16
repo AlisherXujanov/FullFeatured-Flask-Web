40c2d443b0124679bec389f62497cce4c2ab69fb
Alisher <alisherxujanov163@gmail.com>
2020-11-16 12:54:19 +0300

    Pagniation

--- main/routes.py
+++ main/routes.py
@@ -11,7 +11,8 @@ from PIL import Image
 @app.route("/")
 @app.route("/home")
 def home():
-    posts = Post.query.order_by(Post.date_posted.desc()).all()
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=3)
     return render_template('home.html', posts=posts)
 
 
@@ -149,6 +150,23 @@ def delete_post(post_id):
     db.session.commit()
     flash('Your post has been Deleted', 'success')
     return redirect(url_for('home'))
+
+
+@app.route("/user/<string:username>")
+def user_posts(username):
+    page = request.args.get('page', 1, type=int)
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = Post.query.filter_by(author=user).\
+        order_by(Post.date_posted.desc()).\
+            paginate(page=page, per_page=3)
+    return render_template('user_posts.html', posts=posts, user=user)
+
+
+
+
+
+
+
 
 
 
